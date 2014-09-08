7d15e593a39df8107e148edfcf635440eee598fe
Amey Chavan <amey.chavan@vrindasoftech.com>
2014-09-08 12:47:35 +0530

    init 1

---
+++ currency.js
@@ -0,0 +1,11 @@
+
+currencytowords(123.56);
+
+
+
+function currencytowords(data) {
+
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+console.log(data)
+console.log("\n_______________________________________________\n")
+}
